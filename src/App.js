c3f05e03dd4de8c24985eeb11d1e0e9a840c40b0
oyhoyhk <oyhoyhk@naver.com>
2021-03-29 20:15:47 +0900

    second commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,17 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return (
+        <div className="App">
+            <h1>소프트웨어 공학 팀 프로젝트</h1>
+            <ul>
+                <li>오윤환</li>
+                <li>전우철</li>
+                <li>이우재</li>
+            </ul>
+        </div>
+    );
 }
 
 export default App;
